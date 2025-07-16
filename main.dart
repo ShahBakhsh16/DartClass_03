68ff5f576fd3274e8b29fefaaad11bc6cca71cbc
Shah Bakhsh <shahbakhshmugheri16@gmail.com>
2025-07-16 22:42:36 +0300

    My first project dart done

---
+++ main.dart
@@ -0,0 +1,65 @@
+// void main() {
+//   print("Hello Dart");
+
+//   bool isAssignmentDone = true;
+
+//   if (isAssignmentDone) {
+//     print("Enter the class");
+//   } else {
+//     print("No enter without assignment");
+//   }
+// }
+// void main() {
+//   int totalAssignment = 5;
+//   int minAssignment = 4;
+//   int assignmentDone = 3;
+
+//   if (assignmentDone >= minAssignment) {
+//     print("Enter the class");
+//   } else {
+//     print("If Not done assignment left the class");
+//   }
+// }
+// void main() {
+//   int totalAssignment = 6;
+//   int minAssignment = 5;
+//   int assignmentDone = 5;
+
+//   if (assignmentDone >= minAssignment) {
+//     print("Enter the class");
+//   } else {
+//     print("Leave the class");
+//   }
+// }
+
+// void main() {
+//   int totalAssignment = 6;
+//   int minAssignmentDone = 4;
+//   int assignmentDone = 4;
+
+//   if (assignmentDone >= minAssignmentDone) {
+//     print("Enter the class room");
+//   } else {
+//     print("Leave the class room");
+//   }
+// }
+
+// void main() {
+//   bool assignementDone = true;
+
+//   if (assignementDone) {
+//     print("Enter the class");
+//   } else {
+//     print("Leave the class");
+//   }
+// }
+
+void main() {
+  bool assignmentDone = true;
+
+  if (assignmentDone) {
+    print("Enter the classs");
+  } else {
+    print("Not allowed without assignement");
+  }
+}
